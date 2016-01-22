7330a99c249febd4263467a3aa7de12721919bdf
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-01-22 12:42:22 -0600

    simple refactor challenge created for CSP

---
+++ refactor_me.rb
@@ -0,0 +1,20 @@
+puts "enter your First Name"
+first_name = gets.chomp
+puts "Enter your Last name"
+last_name = gets.chomp
+puts "enter your age"
+age = gets
+if age <= 15
+puts "Enjoy life while you're young kid."
+elsif age = 16
+puts " What is your favorite car"
+car = gets.chomp
+puts "better get a job, a #{car} doesn't buy itself"
+elseif age > 16
+puts "What do you want to be when you grow up?"
+dream = "gets.chomp"
+puts "I'll see you in 5 years, " + "first_name" + last_name. + "The #{dream}."
+elsif > 18
+"Woo! #{first_name} is an adult!"
+"Watch out world!"
+end
