f9ff9067ab64f66cfd2ed75d568fc36c908e12e8
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-01-22 13:01:27 -0600

    added string  methods

--- refactor_me.rb
+++ refactor_me.rb
@@ -9,12 +9,12 @@ puts "Enjoy life while you're young kid."
 elsif age = 16
 puts " What is your favorite car"
 car = gets.chomp
-puts "better get a job, a #{car} doesn't buy itself"
+puts "better get a job, a #{car.downcase} doesn't buy itself"
 elseif age > 16
 puts "What do you want to be when you grow up?"
 dream = "gets.chomp"
-puts "I'll see you in 5 years, " + "first_name" + last_name. + "The #{dream}."
+puts "I'll see you in 5 years, " + "first_name.capital" + last_name.capital. + "The #{dream}.".upcase
 elsif > 18
-"Woo! #{first_name} is an adult!"
+"Woo! #{first_name.capitalize} is an adult!"
 "Watch out world!"
 end
