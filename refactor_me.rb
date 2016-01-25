735caa8260c7758b3cae76a67ba194d60b8aeabb
Tara Campbell <tac1998@gmail.com>
2016-01-25 09:10:02 -0600

    fixed broken code

--- refactor_me.rb
+++ refactor_me.rb
@@ -1,20 +1,20 @@
-puts "enter your First Name"
+puts "enter your first name"
 first_name = gets.chomp
-puts "Enter your Last name"
+puts "Enter your last name"
 last_name = gets.chomp
 puts "enter your age"
-age = gets
+age = gets.chomp
+age = age.to_i
 if age <= 15
-puts "Enjoy life while you're young kid."
-elsif age = 16
-puts " What is your favorite car"
+puts "enjoy life while you're young kid."
+elsif age == 16
+puts "what is your favorite car"
 car = gets.chomp
 puts "better get a job, a #{car.downcase} doesn't buy itself"
-elseif age > 16
-puts "What do you want to be when you grow up?"
-dream = "gets.chomp"
-puts "I'll see you in 5 years, " + "first_name.capital" + last_name.capital. + "The #{dream}.".upcase
-elsif > 18
-"Woo! #{first_name.capitalize} is an adult!"
-"Watch out world!"
+elsif age == 17
+puts "what do you want to be when you grow up?"
+dream = gets.chomp
+puts "I'll see you in 5 years, " + first_name.capitalize + " " + last_name.capitalize + " the #{dream.upcase}"
+elsif age >= 18
+puts "Woo! #{first_name.capitalize} is an adult!" + " Watch out world!"
 end
